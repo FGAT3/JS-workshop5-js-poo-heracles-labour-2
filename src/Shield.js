8826d7fbaf73687b9e25acc12d3e77927792c1a1
FGAT3 <feliciengamelin@yahoo.fr>
2024-05-30 12:22:32 +0200

    finished workshop i hope

---
+++ src/Shield.js
@@ -0,0 +1,7 @@
+class Shield {
+  constructor(defense) {
+    this.defense = defense || 10;
+  }
+}
+
+module.exports = Shield;
---
+++ src/Weapon.js
@@ -0,0 +1,7 @@
+class Weapon {
+  constructor(damage) {
+    this.damage = damage || 10;
+  }
+}
+
+module.exports = Weapon;
